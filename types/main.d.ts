30bb95a5c4df166df9dae08899fb9c8b527afee1
ehmicky <ehmicky@gmail.com>
2022-11-20 10:04:45 +0100

    Update documentation

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,9 +1,36 @@
 type NormalizeError<ErrorArg> = ErrorArg extends Error ? ErrorArg : Error
 
 /**
+ * Appends `message` to `error.message`. If `message` ends with `:` or `:\n`,
+ * prepends it instead.
+ *
+ * Returns `error`. If `error` is not an `Error` instance, it is converted to
+ * one.
  *
  * @example
  * ```js
+ * wrapErrorMessage(new Error('Message.'), 'Additional message.')
+ * // Error: Message.
+ * // Additional message.
+ *
+ * wrapErrorMessage(new Error('Message.'), 'Additional message:')
+ * // Error: Additional message: Message.
+ *
+ * wrapErrorMessage(new Error('Message.'), 'Additional message:\n')
+ * // Error: Additional message:
+ * // Message.
+ *
+ * wrapErrorMessage(new Error('Message.'), '')
+ * // Error: Message.
+ *
+ * const invalidError = 'Message.'
+ * wrapErrorMessage(invalidError, 'Additional message.')
+ * // Error: Message.
+ * // Additional message.
+ *
+ * wrapErrorMessage(new Error('  Message with spaces  '), '  Additional message  ')
+ * // Error: Message with spaces
+ * // Additional message
  * ```
  */
 export default function wrapErrorMessage<ErrorArg>(
